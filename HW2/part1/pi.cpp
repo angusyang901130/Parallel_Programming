88d98eeef88f4bc2e0a88b8d4d93d40d1ce574e1
angusyang901130 <ccy.angus1130@gmail.com>
2023-10-30 23:49:11 +0800

    pi.cpp -> fix thread function counter

--- HW2/part1/pi.cpp
+++ HW2/part1/pi.cpp
@@ -2,29 +2,45 @@
 #include <pthread.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <cmath>
+#include <climits>
+#include <iomanip>
 
 using namespace std;
 
-double n_in_circle;
+
 int n_thread;
 long long n_toss;
+long long n_in_circle = 0;
 long long n_toss_per_thread;
 
 pthread_mutex_t mutex;
 
 void* random_toss(void* rank){
     long long cur_rank = (long long)rank;
+    // cout << "cur_rank: " << cur_rank << endl;
+
     long long local_n_in_circle = 0;
     double dist_squared = 0;
 
     struct drand48_data buffer;
     srand48_r(time(NULL), &buffer);
-    long int value_x;
-    long int value_y;
 
-    for(int toss = rank*n_toss_per_thread; toss < n_toss; toss++){
-        x = drand48_r(&buffer, &value_x);
-        y = drand48_r(&buffer, &value_y);
+    long value_x;
+    long value_y;
+
+    long long iter_start = cur_rank * n_toss_per_thread;
+    long long iter_fin = iter_start + n_toss_per_thread <= n_toss ? iter_start + n_toss_per_thread : n_toss;
+
+    for(long long toss = iter_start; toss < iter_fin; toss++){
+        mrand48_r(&buffer, &value_x);
+        mrand48_r(&buffer, &value_y);
+
+        // if(value_x > INT_MAX || value_x < -INT_MAX || value_y > INT_MAX || value_y < -INT_MAX)
+        //     cout << "range error !!" << endl;
+
+        double x = ((double)value_x) / INT_MAX;
+        double y = ((double)value_y) / INT_MAX;
         
         dist_squared = x * x + y * y;
 
@@ -49,10 +65,15 @@ int main(int argc, char* argv[]){
 
     long thread;
 
-    n_toss = strtoll([argv[2]]);
+    n_toss = strtoll(argv[2], NULL, 10);
+    // cout << "n_toss: " << n_toss << endl;
+
     n_in_circle = 0;
     n_thread = atoi(argv[1]);
+    // cout << "n_thread: " << n_thread << endl;
+
     n_toss_per_thread = ceil(n_toss / (double)n_thread);
+    // cout << "n_toss_per_thread: " << n_toss_per_thread << endl;
 
     pthread_t* thread_handles = (pthread_t*)calloc(n_thread, sizeof(pthread_t));
     pthread_mutex_init(&mutex, NULL);
@@ -63,12 +84,12 @@ int main(int argc, char* argv[]){
     for(thread = 0; thread < n_thread; thread++)
         pthread_join(thread_handles[thread], NULL);
 
-    pi_estimate = 4 * n_in_circle /(( double ) n_of_toss);
+    double pi_estimate = 4 * n_in_circle /(( double ) n_toss);
 
     pthread_mutex_destroy(&mutex);
     free(thread_handles);
 
-    cout << pi_estimate << endl;
+    cout << setprecision(8) << pi_estimate << endl;
 
     return 0;
 }
