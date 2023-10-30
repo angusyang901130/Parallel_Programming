0bfd794482770ebb9460447e850d5a8f8a4e28c3
angusyang901130 <ccy.angus1130@gmail.com>
2023-10-31 03:18:02 +0800

    pi.cpp -> vectorized successfully pass time limit

--- HW2/part1/pi.cpp
+++ HW2/part1/pi.cpp
@@ -1,13 +1,16 @@
 #include <iostream>
 #include <pthread.h>
-#include <unistd.h>
 #include <stdlib.h>
-#include <cmath>
 #include <climits>
 #include <iomanip>
+#include <cmath>
+#include <x86intrin.h>
+
+#pragma GCC target("avx2")
 
 using namespace std;
 
+const int VECTOR_SIZE = 8;
 
 int n_thread;
 long long n_toss;
@@ -16,36 +19,56 @@ long long n_toss_per_thread;
 
 pthread_mutex_t mutex;
 
-void* random_toss(void* rank){
+void* random_toss(void* rank) {
     long long cur_rank = (long long)rank;
-    // cout << "cur_rank: " << cur_rank << endl;
 
     long long local_n_in_circle = 0;
-    double dist_squared = 0;
+
+    __m256 v_max = _mm256_set1_ps(1);
+    __m256 one_v = _mm256_set1_ps(INT_MAX);
 
     struct drand48_data buffer;
     srand48_r(time(NULL), &buffer);
 
-    long value_x;
-    long value_y;
+    long values_x[VECTOR_SIZE];
+    long values_y[VECTOR_SIZE];
 
     long long iter_start = cur_rank * n_toss_per_thread;
     long long iter_fin = iter_start + n_toss_per_thread <= n_toss ? iter_start + n_toss_per_thread : n_toss;
 
-    for(long long toss = iter_start; toss < iter_fin; toss++){
-        mrand48_r(&buffer, &value_x);
-        mrand48_r(&buffer, &value_y);
-
-        // if(value_x > INT_MAX || value_x < -INT_MAX || value_y > INT_MAX || value_y < -INT_MAX)
-        //     cout << "range error !!" << endl;
-
-        double x = ((double)value_x) / INT_MAX;
-        double y = ((double)value_y) / INT_MAX;
+    for (long long toss = iter_start; toss < iter_fin; toss += VECTOR_SIZE) {
+        // Generate VECTOR_SIZE random integers in parallel
         
-        dist_squared = x * x + y * y;
+        for (int i = 0; i < VECTOR_SIZE; ++i) {
+            if(toss+i < iter_fin){
+                mrand48_r(&buffer, &values_x[i]);
+                mrand48_r(&buffer, &values_y[i]);
+            }else{
+                values_x[i] = INT_MAX;
+                values_y[i] = INT_MAX;
+            }
+        }
 
-        if (dist_squared <= 1)
-            local_n_in_circle++;
+        __m256i i32_x = _mm256_set_epi32(values_x[7], values_x[6], values_x[5], values_x[4], values_x[3], values_x[2], values_x[1], values_x[0]);
+        __m256i i32_y = _mm256_set_epi32(values_y[7], values_y[6], values_y[5], values_y[4], values_y[3], values_y[2], values_y[1], values_y[0]);
+
+        __m256 ps_x = _mm256_cvtepi32_ps(i32_x);
+        __m256 ps_y = _mm256_cvtepi32_ps(i32_y);
+        
+        // __m256 pd_x = _mm256_cvtps_ps(ps_x);
+        // __m256 pd_y = _mm256_cvtps_ps(ps_y);
+
+        __m256 v_x = _mm256_div_ps(ps_x, one_v);
+        __m256 v_y = _mm256_div_ps(ps_y, one_v);
+
+        // Convert to doubles and calculate the square of the distances in parallel
+        __m256 v_x_squared = _mm256_mul_ps(v_x, v_x);
+        __m256 v_y_squared = _mm256_mul_ps(v_y, v_y);
+        __m256 v_squared = _mm256_add_ps(v_x_squared, v_y_squared);
+
+        // Check if the squared distances are less than or equal to 1 (in parallel)
+        __m256 v_cmp = _mm256_cmp_ps(v_max, v_squared, _CMP_GT_OQ);
+        local_n_in_circle += _mm_popcnt_u32(_mm256_movemask_ps(v_cmp));
     }
 
     pthread_mutex_lock(&mutex);
@@ -53,12 +76,10 @@ void* random_toss(void* rank){
     pthread_mutex_unlock(&mutex);
 
     return NULL;
-
 }
 
-int main(int argc, char* argv[]){
-
-    if(argc != 3){
+int main(int argc, char* argv[]) {
+    if (argc != 3) {
         cout << "Argument is not enough, exiting ..." << endl;
         return 0;
     }
@@ -66,32 +87,25 @@ int main(int argc, char* argv[]){
     long thread;
 
     n_toss = strtoll(argv[2], NULL, 10);
-    // cout << "n_toss: " << n_toss << endl;
-
     n_in_circle = 0;
     n_thread = atoi(argv[1]);
-    // cout << "n_thread: " << n_thread << endl;
-
     n_toss_per_thread = ceil(n_toss / (double)n_thread);
-    // cout << "n_toss_per_thread: " << n_toss_per_thread << endl;
 
     pthread_t* thread_handles = (pthread_t*)calloc(n_thread, sizeof(pthread_t));
     pthread_mutex_init(&mutex, NULL);
 
-    for(thread = 0; thread < n_thread; thread++)
+    for (thread = 0; thread < n_thread; thread++)
         pthread_create(&thread_handles[thread], NULL, random_toss, (void*)thread);
 
-    for(thread = 0; thread < n_thread; thread++)
+    for (thread = 0; thread < n_thread; thread++)
         pthread_join(thread_handles[thread], NULL);
 
-    double pi_estimate = 4 * n_in_circle /(( double ) n_toss);
+    float pi_estimate = 4 * n_in_circle / ((double)n_toss);
 
     pthread_mutex_destroy(&mutex);
     free(thread_handles);
 
-    cout << setprecision(8) << pi_estimate << endl;
+    cout << "Estimated Value of PI: " << setprecision(8) << pi_estimate << endl;
 
     return 0;
 }
-
-
