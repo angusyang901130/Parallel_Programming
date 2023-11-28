b958133184e9b1055847f542b2efcd286fb417e5
angusyang901130 <ccy.angus1130@gmail.com>
2023-11-28 22:53:32 +0800

    pi_block_tree

--- HW4/src/pi_block_tree.cc
+++ HW4/src/pi_block_tree.cc
@@ -16,12 +16,55 @@ int main(int argc, char **argv)
     // ---
 
     // TODO: MPI init
+    MPI_Status status;
+    unsigned int seed = world_rank;
+    int tag = 0;
+
+    long int count = 0;
+    double rand_bias = RAND_MAX / 2;
+    char msg[100];
+
+    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
 
     // TODO: binary tree redunction
+    int tmp_rank = world_rank;
+    int level = 1;
 
+    double x, y, dist_sq;
+    for(int i = world_rank; i < tosses; i += world_size){
+        x = (rand_r(&seed) - rand_bias) / rand_bias;
+        y = (rand_r(&seed) - rand_bias) / rand_bias;
+
+        dist_sq = x * x + y * y;
+
+        if(dist_sq <= 1)
+            count++;
+    }
+
+    while(tmp_rank % 2 == 0 && level < world_size){
+        // printf("world rank: %d, level: %d, recv_rank: %d, tmp_rank: %d\n", world_rank, level, world_rank+level, tmp_rank);
+        MPI_Recv(msg, 100, MPI_LONG, world_rank+level, tag, MPI_COMM_WORLD, &status);
+
+        long int recv_cnt;
+        sscanf(msg, "%ld", &recv_cnt);
+
+        count += recv_cnt;
+
+        tmp_rank /= 2;
+        level *= 2;
+    }
+
+    if(world_rank > 0){
+        // printf("world_rank: %d\n", world_rank);
+        sprintf(msg, "%ld", count);
+        MPI_Send(msg, strlen(msg)+1, MPI_LONG, world_rank-level, tag, MPI_COMM_WORLD);
+    }
+    
     if (world_rank == 0)
     {
         // TODO: PI result
+        pi_result = 4 * count / (double)tosses;
 
         // --- DON'T TOUCH ---
         double end_time = MPI_Wtime();
--- HW4/src/test.sh
+++ HW4/src/test.sh
@@ -1,6 +1,28 @@
 #!/bin/sh
 
 # 1.2.1
+echo "===== Task 1.2.1 ====="
 mpicxx pi_block_linear.cc -o pi_block_linear
-parallel-scp -h hosts -r ~/Parallel_Programming/HW4/src/pi_block_linear ~/pi_block_linear
-mpirun -np 4 --hostfile hosts pi_block_linear 1000000000
+parallel-scp -h hosts -r ~/Parallel_Programming/HW4/src/pi_block_linear ~/pi_block_linear >/dev/null
+echo "<<stu>>"
+mpirun -np 4 --hostfile hosts pi_block_linear 1000000000 2>/dev/null
+echo "<<ref>>"
+mpirun -np 4 --hostfile hosts /home/HW4/ref/pi_block_linear 1000000000 2>/dev/null
+
+#1.2.2
+echo "===== Task 1.2.2 ====="
+mpicxx pi_block_tree.cc -o pi_block_tree
+parallel-scp -h hosts -r ~/Parallel_Programming/HW4/src/pi_block_tree ~/pi_block_tree >/dev/null
+echo "<<stu>>"
+mpirun -np 4 --hostfile hosts pi_block_tree 1000000000 2>/dev/null
+echo "<<ref>>"
+mpirun -np 4 --hostfile hosts /home/HW4/ref/pi_block_tree 1000000000 2>/dev/null
+
+#1.2.3
+echo "===== Task 1.2.3 ====="
+mpicxx pi_nonblock_linear.cc -o pi_nonblock_linear
+parallel-scp -h hosts -r ~/Parallel_Programming/HW4/src/pi_nonblock_linear ~/pi_nonblock_linear >/dev/null
+echo "<<stu>>"
+mpirun -np 4 --hostfile hosts pi_nonblock_linear 1000000000 2>/dev/null
+echo "<<ref>>"
+mpirun -np 4 --hostfile hosts /home/HW4/ref/pi_nonblock_linear 1000000000 2>/dev/null
